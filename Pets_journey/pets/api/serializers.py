bc05211f6f1a45f2a202cfb06f23f12698a5afe9
Redaloukil <loukil.red@gmail.com>
2019-08-24 12:10:37 +0100

    collect static files succefully

---
+++ Pets_journey/pets/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+from ..models import Dog , Cat
+
+
+class DogSerializer(ModelSerializer):
+    pass
+
+class CatSerilizer(ModelSerializer):
+    pass
