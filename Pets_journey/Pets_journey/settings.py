f65bea2c0abe361da70af4b22a48c56e43c129c9
Redaloukil <loukil.red@gmail.com>
2019-08-25 16:30:34 +0100

    remove static dirs

--- Pets_journey/Pets_journey/settings.py
+++ Pets_journey/Pets_journey/settings.py
@@ -131,9 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), 'static')) 
-
-STATICFILES_DIRS = (os.path.join('static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 AUTH_USER_MODEL = "users.User" 
 
